c9a619ba5929df9e14057cf6ee602496f1411e96
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-25 23:16:56 +0100

    (feature: implementing jwt auth filter): implement generate token method

--- src/main/java/com/sentinelql/springsecurityjwtmongodb/config/JwtService.java
+++ src/main/java/com/sentinelql/springsecurityjwtmongodb/config/JwtService.java
@@ -2,12 +2,16 @@ package com.sentinelql.springsecurityjwtmongodb.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -16,6 +20,9 @@ public class JwtService {
     @Value("${app.security.jwt.secret-key}")
     private String secretKey;
 
+    @Value("${app.security.jwt.expiration}")
+    private long jwtExpiration;
+
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
@@ -23,6 +30,24 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(Map.of(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
