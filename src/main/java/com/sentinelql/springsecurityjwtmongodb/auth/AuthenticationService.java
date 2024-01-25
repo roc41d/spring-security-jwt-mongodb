ece6e56e8025d16f169545d70a207a4bfa2eae73
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-26 00:18:44 +0100

    (feature: authentication controller): implement authentication method

--- src/main/java/com/sentinelql/springsecurityjwtmongodb/auth/AuthenticationService.java
+++ src/main/java/com/sentinelql/springsecurityjwtmongodb/auth/AuthenticationService.java
@@ -5,6 +5,9 @@ import com.sentinelql.springsecurityjwtmongodb.user.Role;
 import com.sentinelql.springsecurityjwtmongodb.user.User;
 import com.sentinelql.springsecurityjwtmongodb.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,7 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
                 .firstname(request.getFirstname())
@@ -32,6 +36,24 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(),
+                            request.getPassword()
+                    )
+            );
+
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(ex.getMessage());
+        }
+
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                .build();
     }
 }
