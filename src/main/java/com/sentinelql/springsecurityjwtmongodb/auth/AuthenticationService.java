0d3bf5642ffde05e38ed47c774da7e5fd83ac2bb
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-26 00:15:08 +0100

    (feature: authentication controller): implement register method

--- src/main/java/com/sentinelql/springsecurityjwtmongodb/auth/AuthenticationService.java
+++ src/main/java/com/sentinelql/springsecurityjwtmongodb/auth/AuthenticationService.java
@@ -1,13 +1,34 @@
 package com.sentinelql.springsecurityjwtmongodb.auth;
 
+import com.sentinelql.springsecurityjwtmongodb.config.JwtService;
+import com.sentinelql.springsecurityjwtmongodb.user.Role;
+import com.sentinelql.springsecurityjwtmongodb.user.User;
+import com.sentinelql.springsecurityjwtmongodb.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
     public AuthenticationResponse register(RegisterRequest request) {
-        return  null;
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
