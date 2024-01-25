e6f0794ec86bd1ad78c1beb297c01208576590fa
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-25 23:41:03 +0100

    (feature: implementing jwt auth filter): update security context holder and complete filter implementation

--- src/main/java/com/sentinelql/springsecurityjwtmongodb/config/JwtAuthenticationFilter.java
+++ src/main/java/com/sentinelql/springsecurityjwtmongodb/config/JwtAuthenticationFilter.java
@@ -41,6 +41,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
+            if (jwtService.isTokenValid(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
+        filterChain.doFilter(request, response);
     }
 }
