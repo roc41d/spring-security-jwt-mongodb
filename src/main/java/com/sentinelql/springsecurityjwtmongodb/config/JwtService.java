9d2a166af4cddae0d8fe40410123c91d2c0346e0
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-25 23:07:27 +0100

    (feature: implementing jwt auth filter): implement getSignInKey method

--- src/main/java/com/sentinelql/springsecurityjwtmongodb/config/JwtService.java
+++ src/main/java/com/sentinelql/springsecurityjwtmongodb/config/JwtService.java
@@ -2,10 +2,19 @@ package com.sentinelql.springsecurityjwtmongodb.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    @Value("${app.security.jwt.secret-key}")
+    private String secretKey;
+
     public String extractUsername(String token) {
         return null;
     }
@@ -17,5 +26,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
