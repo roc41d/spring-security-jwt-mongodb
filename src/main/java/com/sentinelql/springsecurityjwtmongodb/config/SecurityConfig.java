302656da055671baaff5dc008b00d28efd724f30
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-25 23:56:46 +0100

    (feature: implementing jwt auth filter): create authentication manager bean

--- src/main/java/com/sentinelql/springsecurityjwtmongodb/config/SecurityConfig.java
+++ src/main/java/com/sentinelql/springsecurityjwtmongodb/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sentinelql.springsecurityjwtmongodb.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,6 +19,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
