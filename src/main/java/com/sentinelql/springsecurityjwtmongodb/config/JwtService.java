8054a0637968d2e1ae89272457bee1562c8a3463
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-25 23:10:11 +0100

    (feature: implementing jwt auth filter): extract single claim from jwt

--- src/main/java/com/sentinelql/springsecurityjwtmongodb/config/JwtService.java
+++ src/main/java/com/sentinelql/springsecurityjwtmongodb/config/JwtService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -17,6 +18,11 @@ public class JwtService {
 
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
